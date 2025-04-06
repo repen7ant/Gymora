1ff4d884da00983e7e16e037ca1ebd92b77424d3
repen7ant <ilyarepentant@icloud.com>
2025-04-06 18:18:36 +0500

    prototype

---
+++ Gymora/Diet.xaml.cs
@@ -0,0 +1,9 @@
+namespace Gymora;
+
+public partial class Diet : ContentPage
+{
+	public Diet()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Gymora/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Gymora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnProfileIconClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
